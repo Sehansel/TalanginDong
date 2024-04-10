ed4e152601a6eced29c5c84044943cb51e9494e5
Theresano Julian Laksono <theresano.laksono@binus.edu>
2024-04-10 22:56:49 +0700

    feat: Revamp register page

--- src/screens/authentication/register.tsx
+++ src/screens/authentication/register.tsx
@@ -4,10 +4,11 @@ import { observer, useLocalObservable } from 'mobx-react-lite';
 import React from 'react';
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
-import { Button, HelperText } from 'react-native-paper';
+import { Button, HelperText, Checkbox, Dialog, Portal, Snackbar } from 'react-native-paper';
 
 import { CustomTextInput } from '../../components/customTextInput';
 import { AuthNavigatorParamList } from '../../navigations/authNavigator';
+import * as AuthService from '../../services/authService';
 import { COLOR } from '../../theme';
 
 interface IRegisterProps {
@@ -17,15 +18,93 @@ interface IRegisterProps {
 export const RegisterScreen: React.FC<IRegisterProps> = observer(function RegisterScreen(props) {
   const { navigation } = props;
   const authStore = useLocalObservable(() => ({
-    email: '',
-    password: '',
+    username: {
+      value: '',
+      isError: false,
+      helperText: '',
+    },
+    email: {
+      value: '',
+      isError: false,
+      helperText: '',
+    },
+    password: {
+      value: '',
+      isError: false,
+      helperText: '',
+    },
+    confirmPassword: {
+      value: '',
+      isError: false,
+      helperText: '',
+    },
+    isChecked: false,
     isSubmited: false,
     loading: false,
+    snackbar: {
+      value: '',
+      visible: false,
+    },
+    dialog: false,
+    setUsername(text: string) {
+      this.username.value = text;
+      if (text.length < 3) {
+        this.username.isError = true;
+        this.username.helperText = 'Must be alteast 3 characters!';
+      } else if (!/^\w+$/.test(text)) {
+        this.username.isError = true;
+        this.username.helperText = 'Must be alphanumeric!';
+      } else {
+        this.username.isError = false;
+        this.username.helperText = '';
+      }
+    },
     setEmail(text: string) {
-      this.email = text;
+      this.email.value = text;
+      if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(text)) {
+        this.email.isError = true;
+        this.email.helperText = 'Email is invalid!';
+      } else {
+        this.email.isError = false;
+        this.email.helperText = '';
+      }
     },
     setPassword(text: string) {
-      this.password = text;
+      this.password.value = text;
+      if (text.length < 8) {
+        this.password.isError = true;
+        this.password.helperText = 'Must be atleast 8 characters!';
+      } else if (!/[A-Z]/.test(text) || !/[a-z]/.test(text)) {
+        this.password.isError = true;
+        this.password.helperText = 'Must include lowercase and uppercase letter!';
+      } else if (!/[0-9]/.test(text)) {
+        this.password.isError = true;
+        this.password.helperText = 'Must include number!';
+      } else {
+        this.password.isError = false;
+        this.password.helperText = '';
+      }
+    },
+    setConfirmPassword(text: string) {
+      this.confirmPassword.value = text;
+      if (this.password.value !== this.confirmPassword.value) {
+        this.confirmPassword.isError = true;
+        this.confirmPassword.helperText = "Password doesn't match!";
+      } else {
+        this.confirmPassword.isError = false;
+        this.confirmPassword.helperText = '';
+      }
+    },
+    setUsernameAlreadyExist() {
+      this.username.isError = true;
+      this.username.helperText = 'Username already exist!';
+    },
+    setEmailAlreadyExist() {
+      this.email.isError = true;
+      this.email.helperText = 'Email already exist!';
+    },
+    setIsChecked(state: boolean) {
+      this.isChecked = state;
     },
     setIsSubmited() {
       this.isSubmited = true;
@@ -33,23 +112,65 @@ export const RegisterScreen: React.FC<IRegisterProps> = observer(function Regist
     setLoading(state: boolean) {
       this.loading = state;
     },
+    setSnackbar(text: string) {
+      this.snackbar.value = text;
+      this.snackbar.visible = true;
+    },
+    hideSnackbar() {
+      this.snackbar.value = '';
+      this.snackbar.visible = false;
+    },
+    setDialogVisible() {
+      this.dialog = true;
+    },
+    hideDialog() {
+      this.dialog = false;
+    },
   }));
 
-  function isEmailInvalid() {
-    return !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(authStore.email) && authStore.isSubmited;
-  }
-
-  function isPasswordInvalid() {
-    return authStore.password === '' && authStore.isSubmited;
-  }
-
-  function submit() {
+  async function submit() {
     authStore.setIsSubmited();
-    if (isEmailInvalid() || isPasswordInvalid()) return;
-    console.log('success');
+    if (
+      authStore.username.isError ||
+      authStore.email.isError ||
+      authStore.password.isError ||
+      authStore.confirmPassword.isError
+    )
+      return;
+    if (!authStore.isChecked) {
+      authStore.setSnackbar('Please indicate that you accept the terms & conditions!');
+      return;
+    }
+    authStore.setLoading(true);
+    try {
+      const response = await AuthService.register(
+        authStore.username.value,
+        authStore.email.value,
+        authStore.password.value,
+      );
+      if (response.ok) {
+        authStore.setDialogVisible();
+      } else if (
+        ['CONNECTION_ERROR', 'NETWORK_ERROR', 'TIMEOUT_ERROR'].includes(response.problem ?? '')
+      ) {
+        authStore.setSnackbar('Please check your network connection before continue!');
+      } else {
+        if (response.data.status === 409 && response.data.reason === 'Username already exist') {
+          authStore.setSnackbar('Username already exist!');
+          authStore.setUsernameAlreadyExist();
+        } else if (response.data.status === 409 && response.data.reason === 'Email already exist') {
+          authStore.setSnackbar('Email already exist!');
+          authStore.setEmailAlreadyExist();
+        } else {
+          authStore.setSnackbar('Unknown error occured!');
+        }
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error: any) {
+      authStore.setSnackbar('Unknown error occured');
+    }
+    authStore.setLoading(false);
   }
-
-  console.log('Re render');
 
   return (
     <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ flexGrow: 1 }}>
@@ -57,35 +178,83 @@ export const RegisterScreen: React.FC<IRegisterProps> = observer(function Regist
         <View style={styles.centerContainer}>
           <Text style={styles.title}>Register</Text>
           <CustomTextInput
-            label='Email'
-            value={authStore.email}
-            onChangeText={(text) => authStore.setEmail(text)}
-            error={isEmailInvalid()}
+            label='Username'
+            value={authStore.username.value}
+            onChangeText={(text) => authStore.setUsername(text)}
+            error={authStore.username.isError}
           />
-          <HelperText style={{ alignSelf: 'flex-start' }} type='error' visible={isEmailInvalid()}>
-            Email is invalid!
+          <HelperText
+            style={{ alignSelf: 'flex-start' }}
+            type='error'
+            visible={authStore.username.isError}
+            padding='none'>
+            {authStore.username.helperText}
+          </HelperText>
+          <CustomTextInput
+            label='Email'
+            value={authStore.email.value}
+            onChangeText={(text) => authStore.setEmail(text)}
+            error={authStore.email.isError}
+            autoCapitalize='none'
+          />
+          <HelperText
+            style={{ alignSelf: 'flex-start' }}
+            type='error'
+            visible={authStore.email.isError}>
+            {authStore.email.helperText}
           </HelperText>
           <CustomTextInput
             isSecureInput
             label='Password'
-            value={authStore.password}
+            value={authStore.password.value}
             onChangeText={(text) => authStore.setPassword(text)}
-            error={isPasswordInvalid()}
+            error={authStore.password.isError}
           />
-          <View style={styles.forgotPasswordContainer}>
-            <HelperText type='error' visible={isPasswordInvalid()}>
-              Password is invalid!
-            </HelperText>
-            <Pressable onPress={() => console.log('test')}>
-              <Text style={{ ...styles.touchableText, fontSize: 12 }}>Forgot Password?</Text>
-            </Pressable>
+          <HelperText
+            style={{ alignSelf: 'flex-start' }}
+            type='error'
+            visible={authStore.password.isError}>
+            {authStore.password.helperText}
+          </HelperText>
+          <CustomTextInput
+            isSecureInput
+            label='Confirm Password'
+            value={authStore.confirmPassword.value}
+            onChangeText={(text) => authStore.setConfirmPassword(text)}
+            error={authStore.confirmPassword.isError}
+          />
+          <HelperText
+            style={{ alignSelf: 'flex-start' }}
+            type='error'
+            visible={authStore.confirmPassword.isError}>
+            {authStore.confirmPassword.helperText}
+          </HelperText>
+          <View
+            style={{
+              flexDirection: 'row',
+              maxWidth: '100%',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+            }}>
+            <Checkbox
+              status={authStore.isChecked ? 'checked' : 'unchecked'}
+              onPress={() => {
+                authStore.setIsChecked(!authStore.isChecked);
+              }}
+              color={COLOR.PRIMARY}
+            />
+            <Text style={{ flexShrink: 1, overflow: 'hidden', marginTop: 7, fontSize: 15 }}>
+              By creating an account you agree to our
+              <Text style={{ color: COLOR.PRIMARY }}> terms & conditions</Text>.
+            </Text>
           </View>
           <Button
             mode='contained'
             buttonColor={COLOR.PRIMARY}
             textColor='white'
             style={styles.button}
-            onPress={submit}>
+            onPress={submit}
+            loading={authStore.loading}>
             Register
           </Button>
           <View style={styles.accountCreateContainer}>
@@ -101,7 +270,7 @@ export const RegisterScreen: React.FC<IRegisterProps> = observer(function Regist
               alignItems: 'center',
               justifyContent: 'center',
               flexDirection: 'column',
-              marginTop: 200,
+              marginTop: 55,
             }}>
             <View style={styles.iconContainer}>
               <Image
@@ -112,6 +281,42 @@ export const RegisterScreen: React.FC<IRegisterProps> = observer(function Regist
             <Text style={{ fontWeight: '600', fontSize: 20 }}>TalanginDong</Text>
           </View>
         </View>
+        <Snackbar
+          visible={authStore.snackbar.visible}
+          onDismiss={authStore.hideSnackbar}
+          action={{
+            label: 'Ok',
+          }}
+          theme={{ colors: { inversePrimary: COLOR.PRIMARY } }}>
+          {authStore.snackbar.value}
+        </Snackbar>
+        <Portal>
+          <Dialog
+            visible={authStore.dialog}
+            dismissable={false}
+            onDismiss={() => {
+              authStore.hideDialog();
+              navigation.navigate('Login');
+            }}>
+            <Dialog.Title>Success</Dialog.Title>
+            <Dialog.Content>
+              <Text>
+                Your account has been registered! To continue, please login using your registered
+                account.
+              </Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button
+                textColor={COLOR.PRIMARY}
+                onPress={() => {
+                  authStore.hideDialog();
+                  navigation.navigate('Login');
+                }}>
+                Ok
+              </Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
         <StatusBar style='auto' />
       </View>
     </KeyboardAwareScrollView>
@@ -153,7 +358,7 @@ const styles = StyleSheet.create({
   button: {
     width: '100%',
     height: 40,
-    marginTop: 60,
+    marginTop: 10,
   },
   touchableText: {
     color: COLOR.PRIMARY,
